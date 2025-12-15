3bae9aacae1db908ef0e122a0896df97099fd077
stoberdl <67390687+stoberdl@users.noreply.github.com>
2025-12-15 13:13:08 -0500

    File updated at 12/15/2025, 1:13:07 PM

--- 1-TwoSum/1-TwoSum.java
+++ 1-TwoSum/1-TwoSum.java
@@ -1,16 +1,16 @@
-// Last updated: 12/14/2025, 5:56:19 PM
-class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> valueToIndex = new HashMap<>();
-        
-        for (int i = 0; i < nums.length; i++) {
-            int complement = target - nums[i];
-            if (valueToIndex.containsKey(complement)) {
-                return new int[]{valueToIndex.get(complement), i};
-            }
-            valueToIndex.put(nums[i], i);
-        }
-        
-        return new int[]{};
-    }
-}
+// Last updated: 12/15/2025, 1:13:07 PM
+1class Solution {
+2    public int[] twoSum(int[] nums, int target) {
+3        //hashmap of value to index, for loop through nums search hashmap for target - nums[i], if found
+4        //return new array size 2 of indices
+5        HashMap<Integer, Integer> numToIndex = new HashMap<>();
+6        for(int i = 0; i < nums.length; i ++){
+7     
+8            if(numToIndex.get(target - nums[i]) != null){
+9                return new int[]{numToIndex.get(target - nums[i]), i};
+10            }
+11             numToIndex.putIfAbsent(nums[i], i);
+12        }
+13        return new int[]{};
+14    }
+15}
