f4cf537355dee8dad6cfcb18718d99fa93d20b6e
stoberdl <67390687+stoberdl@users.noreply.github.com>
2025-12-18 12:48:48 -0500

    File updated at 12/18/2025, 12:48:47 PM

--- 49-GroupAnagrams/49-GroupAnagrams.java
+++ 49-GroupAnagrams/49-GroupAnagrams.java
@@ -1,17 +1,20 @@
-// Last updated: 12/17/2025, 3:11:55 PM
+// Last updated: 12/18/2025, 12:48:47 PM
 1class Solution {
 2    public List<List<String>> groupAnagrams(String[] strs) {
-3        return new ArrayList<>(
-4                Arrays.stream(strs)
-5                        .collect(
-6                                Collectors.groupingBy(str -> {
-7                                    HashMap<Character, Integer> charToCountMap = new HashMap<>();
-8                                    for (int i = 0; i < str.length(); i++) {
-9                                        charToCountMap.put(str.charAt(i),
-10                                                charToCountMap.getOrDefault(str.charAt(i), 0) + 1);
-11                                    }
-12                                    return charToCountMap;
-13                                }))
-14                        .values());
-15    }
-16}
+3            //for each loop through strs , sorted, add to hashmap of sorted string to list of original strings?, go through hasmap and construct list of the lists to return
+4            HashMap<String, List<String>> sortedToOrigStringList = new HashMap<>();
+5            for(String str: strs){
+6                char[] strChars = str.toCharArray(); 
+7                Arrays.sort(strChars);
+8                String key = new String(strChars);
+9                List<String> strList = sortedToOrigStringList.getOrDefault(key, new ArrayList<>());
+10                strList.add(str);
+11                sortedToOrigStringList.put(key, strList);
+12            }
+13            List<List<String>> groupedAnagrams = new ArrayList<>();
+14            for(List<String> groupedStrs:  sortedToOrigStringList.values()){
+15                groupedAnagrams.add(groupedStrs);
+16            }
+17            return groupedAnagrams;
+18    }
+19}
