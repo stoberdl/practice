5da8c1109cd602f2fa3a34e80a4c5c15fbdce64b
stoberdl <67390687+stoberdl@users.noreply.github.com>
2025-12-27 12:00:20 -0500

    File updated at 12/27/2025, 12:00:20 PM

--- 189-RotateArray/189-RotateArray.java
+++ 189-RotateArray/189-RotateArray.java
@@ -1,11 +1,14 @@
-// Last updated: 12/14/2025, 5:55:56 PM
-class Solution {
-    public void rotate(int[] nums, int k) {
-      
-        int[] numsCopy = nums.clone();
-        for(int i = 0; i < nums.length; i++){
-            int position = (i + k) % nums.length;
-            nums[position] = numsCopy[i];
-        }
-    }
-}
+// Last updated: 12/27/2025, 12:00:20 PM
+1class Solution {
+2    public void rotate(int[] nums, int k) {
+3      
+4        int[] numsPreRotate = nums.clone();
+5        //take original array for reference, using modulus in someway to rotate steps in one pass in place
+6        for(int i = 0; i <nums.length; i++){
+7           // length 7, index 0, k=3: 0 + 3 % 7 = 3
+8            // index 5: (5 + 3) % 7 = 1
+9            // so (i+k) % array length = index of one pass rotation
+10            nums[   ( (i+k) % nums.length )]= numsPreRotate[i];
+11        }
+12    }
+13}
