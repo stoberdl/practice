22a464c638e9917194e0913ac1239755551d6013
stoberdl <67390687+stoberdl@users.noreply.github.com>
2025-12-28 14:53:23 -0500

    File updated at 12/28/2025, 2:53:22 PM

--- 55-JumpGame/55-JumpGame.java
+++ 55-JumpGame/55-JumpGame.java
@@ -1,14 +1,21 @@
-// Last updated: 12/28/2025, 1:51:10 PM
+// Last updated: 12/28/2025, 2:53:22 PM
 1class Solution {
-2    public boolean canJump(int[] nums) {
-3        //READ first index value and up to its jump distance values, maybe a loop(might be bad complexity) see if reaches end on each
-4        //what if went through loop one time, calculate nums to see if = length, needs a reachable check, recursive method?
-5        int goalIndex = nums.length -1;
-6        for (int i = nums.length-1; i >= 0; i--) {
-7            if(nums[i] + i >= goalIndex ){
-8                goalIndex = i;
-9            }
-10        }
-11        return goalIndex == 0;
-12    }
-13}
+2    public int jump(int[] nums) {
+3        if (nums.length <= 1) {
+4            return 0;
+5        }
+6        int jumpCounter = 0;
+7        int currentEnd = 0;
+8        int farthestJump = 0;
+9
+10        for (int i = 0; i < nums.length; i++) {
+11            farthestJump = Math.max(farthestJump, i + nums[i]);
+12            if (i < nums.length -1 && currentEnd == i) {
+13                jumpCounter++;
+14                currentEnd = farthestJump;
+15            }
+16        }
+17
+18        return jumpCounter;
+19    }
+20}
