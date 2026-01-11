527536aff8a8b22cd6e3856bfe6ed47be4a38703
stoberdl <67390687+stoberdl@users.noreply.github.com>
2026-01-11 11:11:23 -0500

    File updated at 1/11/2026, 11:11:22 AM

--- 125-ValidPalindrome/125-ValidPalindrome.java
+++ 125-ValidPalindrome/125-ValidPalindrome.java
@@ -1,28 +1,33 @@
-// Last updated: 12/14/2025, 5:56:03 PM
-class Solution {
-    public boolean isPalindrome(String s) {
-        // String sR = new StringBuilder(s.replaceAll("[^a-zA-Z0-9]", "")).reverse().toString();
-        // return s.replaceAll("[^a-zA-Z0-9]", "").compareToIgnoreCase(sR) == 0;
-        //while loop, left and right converge , pause if bad char
-        int left = 0;
-        int right = s.length() -1;
-        while(left < right){
-            char leftChar = s.charAt(left);
-            char rightChar = s.charAt(right);
-            if(!Character.isLetterOrDigit(leftChar)){
-                left++;
-                continue;
-            }else if(!Character.isLetterOrDigit(rightChar)){
-                right--;
-                continue;
-            }
-
-            if(Character.toLowerCase(leftChar) != Character.toLowerCase(rightChar) ){
-                    return false;
-            }
-            left++;
-            right--;
-        }
-        return true;
-    }
-}
+// Last updated: 1/11/2026, 11:11:22 AM
+1class Solution {
+2    public boolean isPalindrome(String s) {
+3        if (s.isEmpty()) {
+4            return true;
+5        }
+6
+7        //no , . 
+8        //for more effiency analyze left and right side with two pointers since they should be equal
+9        //just clean or skip if not alphanumeric
+10        int left = 0;
+11        int right = s.length() - 1;
+12
+13        while (left < right) {
+14            char lChar = s.charAt(left);
+15            char rChar = s.charAt(right);
+16            if (!Character.isLetterOrDigit(lChar)) {
+17                left++;
+18                continue;
+19            }
+20            if (!Character.isLetterOrDigit(rChar)) {
+21                right--;
+22                continue;
+23            }
+24            if (Character.toLowerCase(lChar) != Character.toLowerCase(rChar)) {
+25                return false;
+26            }
+27            left++;
+28            right--;
+29        }
+30        return true;
+31    }
+32}
