90b261f76c8fca0fff55c1f4640c78cf132505f2
stoberdl <67390687+stoberdl@users.noreply.github.com>
2026-01-16 15:08:10 -0500

    File updated at 1/16/2026, 3:08:10 PM

--- 242-ValidAnagram/242-ValidAnagram.java
+++ 242-ValidAnagram/242-ValidAnagram.java
@@ -1,19 +1,16 @@
-// Last updated: 12/16/2025, 1:10:49 PM
+// Last updated: 1/16/2026, 3:08:10 PM
 1class Solution {
 2    public boolean isAnagram(String s, String t) {
-3        //loop through string, add to hashmaps and increment existing valujes, new loop to check counts, compare hash maps actually?
+3        //hashmap for holding letters to num of letters, if equal at end then true;
 4        if (s.length() != t.length()) {
 5            return false;
 6        }
-7        HashMap<Character, Integer> tCharToCount = new HashMap<>();
-8        HashMap<Character, Integer> sCharToCount = new HashMap<>();
+7        HashMap<Character, Integer> sCharToCountMap = new HashMap<>();
+8        HashMap<Character, Integer> tCharToCountMap = new HashMap<>();
 9        for (int i = 0; i < s.length(); i++) {
-10            sCharToCount.put(s.charAt(i), sCharToCount.getOrDefault(s.charAt(i), 0) + 1);
-11            tCharToCount.put(t.charAt(i), tCharToCount.getOrDefault(t.charAt(i), 0) + 1);
+10            sCharToCountMap.put(s.charAt(i), sCharToCountMap.getOrDefault(s.charAt(i), 0) + 1);
+11            tCharToCountMap.put(t.charAt(i), tCharToCountMap.getOrDefault(t.charAt(i), 0) + 1);
 12        }
-13        if (tCharToCount.equals(sCharToCount)) {
-14            return true;
-15        }
-16        return false;
-17    }
-18}
+13        return sCharToCountMap.equals(tCharToCountMap);
+14    }
+15}
