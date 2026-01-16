7ae9d2686c249cb5cf9f2356ad82f2f042735833
stoberdl <67390687+stoberdl@users.noreply.github.com>
2026-01-16 15:19:41 -0500

    File updated at 1/16/2026, 3:19:41 PM

--- 242-ValidAnagram/242-ValidAnagram.java
+++ 242-ValidAnagram/242-ValidAnagram.java
@@ -1,16 +1,21 @@
-// Last updated: 1/16/2026, 3:08:10 PM
+// Last updated: 1/16/2026, 3:19:41 PM
 1class Solution {
 2    public boolean isAnagram(String s, String t) {
-3        //hashmap for holding letters to num of letters, if equal at end then true;
-4        if (s.length() != t.length()) {
-5            return false;
-6        }
-7        HashMap<Character, Integer> sCharToCountMap = new HashMap<>();
-8        HashMap<Character, Integer> tCharToCountMap = new HashMap<>();
+3        //more optimized solution using "voting" add for one subtract for another, if letter equals 0 for each letter
+4        //then they are equal, handle unicode
+5        if (s.length() != t.length()) {
+6            return false;
+7        }
+8        HashMap<Character, Integer> stCharToVoteMap = new HashMap<>();//s ++, t --
 9        for (int i = 0; i < s.length(); i++) {
-10            sCharToCountMap.put(s.charAt(i), sCharToCountMap.getOrDefault(s.charAt(i), 0) + 1);
-11            tCharToCountMap.put(t.charAt(i), tCharToCountMap.getOrDefault(t.charAt(i), 0) + 1);
+10            stCharToVoteMap.put(s.charAt(i), stCharToVoteMap.getOrDefault(s.charAt(i), 0) + 1);
+11            stCharToVoteMap.put(t.charAt(i), stCharToVoteMap.getOrDefault(t.charAt(i), 0) - 1);
 12        }
-13        return sCharToCountMap.equals(tCharToCountMap);
-14    }
-15}
+13        for(Integer vote: stCharToVoteMap.values()){
+14            if(vote != 0){
+15                return false;
+16            }
+17        }
+18        return true;
+19    }
+20}
