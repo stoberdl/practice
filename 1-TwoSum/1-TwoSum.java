3cb782c379d47403f9b386351e61d550990288d6
stoberdl <67390687+stoberdl@users.noreply.github.com>
2026-01-16 14:50:31 -0500

    File updated at 1/16/2026, 2:50:31 PM

--- 1-TwoSum/1-TwoSum.java
+++ 1-TwoSum/1-TwoSum.java
@@ -1,16 +1,15 @@
-// Last updated: 12/15/2025, 1:13:07 PM
+// Last updated: 1/16/2026, 2:50:31 PM
 1class Solution {
 2    public int[] twoSum(int[] nums, int target) {
-3        //hashmap of value to index, for loop through nums search hashmap for target - nums[i], if found
-4        //return new array size 2 of indices
-5        HashMap<Integer, Integer> numToIndex = new HashMap<>();
-6        for(int i = 0; i < nums.length; i ++){
-7     
+3        // hashmap of array for lookup of num to index, fast
+4        // if num bigger than target skip loop
+5        // on each iteration add to hashmap of the seen indices, next lookup hashmap of target - num[i] see if get works from hashmap, if so, good!
+6        HashMap<Integer, Integer> numToIndex = new HashMap<>();
+7        for(int i = 0; i < nums.length; i++){
 8            if(numToIndex.get(target - nums[i]) != null){
 9                return new int[]{numToIndex.get(target - nums[i]), i};
 10            }
-11             numToIndex.putIfAbsent(nums[i], i);
+11            numToIndex.put(nums[i], i);
 12        }
-13        return new int[]{};
-14    }
-15}
+13        return nums;//somethign went wrong
+14}}
